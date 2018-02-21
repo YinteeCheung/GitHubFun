1297b6cd796330841fdb0499184e71097fb2e0b3
Web Developer <webdeveloper@ac02qn00pgl29.local>
2018-02-21 12:45:34 -0500

    Initial Commit

---
+++ GItHubFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GItHubFun
+//
+//  Created by Web Developer on 2/21/18.
+//  Copyright © 2018 Web Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
